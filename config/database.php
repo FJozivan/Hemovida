197cb25dee6bbf1531a9b888ea9ecad15c8925ab
Francisco Jozivan <josivamfreire.fj@gmail.com>
2018-10-01 14:56:40 -0300

    Teste de banco remoto_2

--- config/database.php
+++ config/database.php
@@ -1,5 +1,13 @@
 <?php
 
+
+    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+    $host = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $database = substr($url["path"], 1);
+
 return [
 
     /*
@@ -14,13 +22,6 @@ return [
     */
 
     // 'default' => env('DB_CONNECTION', 'mysql'),
-
-    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-
-    $host = $url["host"];
-    $username = $url["user"];
-    $password = $url["pass"];
-    $database = substr($url["path"], 1);
 
     /*
     |--------------------------------------------------------------------------
