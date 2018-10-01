c30899dc8c269685ead759f5b172e5dda0d0681f
Francisco Jozivan <josivamfreire.fj@gmail.com>
2018-10-01 14:48:34 -0300

    Teste de banco remoto

--- config/database.php
+++ config/database.php
@@ -13,7 +13,14 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    // 'default' => env('DB_CONNECTION', 'mysql'),
+
+    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+    $host = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $database = substr($url["path"], 1);
 
     /*
     |--------------------------------------------------------------------------
@@ -41,11 +48,16 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', ''),
-            'username' => env('DB_USERNAME', ''),
-            'password' => env('DB_PASSWORD', ''),
+            // 'host' => env('DB_HOST', '127.0.0.1'),
+            // 'port' => env('DB_PORT', '3306'),
+            // 'database' => env('DB_DATABASE', ''),
+            // 'username' => env('DB_USERNAME', ''),
+            // 'password' => env('DB_PASSWORD', ''),
+            'host' => $host,
+            'port' => '3306',
+            'database' => $database,
+            'username' => $username,
+            'password' => $password,
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
