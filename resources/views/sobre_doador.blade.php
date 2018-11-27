3cf583c5ffa8ab83886847ed18a31bab8e6d65c5
AderbalFilho <aderbalcrato@yahoo.com.br>
2018-11-27 20:27:16 -0300

    fix(sobre_doador): Melhoria na visualização em telas menores

--- resources/views/sobre_doador.blade.php
+++ resources/views/sobre_doador.blade.php
@@ -6,6 +6,11 @@
 @extends('layouts.cabecalho')
 @stop
 @section('corpo')
+<style>
+    .btn {
+        white-space: normal !important;
+    }    
+</style>
 <div class="mt-3" style="color: transparent">.</div>
 <div class="container mt-4" style="background-color: white; border-radius: 10px; box-shadow: 0px 0px 0px 0.4px; margin-top: 10px; margin-bottom: 10px;">
     <div class="row mr-2 ml-2">
@@ -383,7 +388,7 @@
         </div>
     </div>
     <div class="row mr-2 ml-2 mt-4">
-        <h2 class="col-sm-12" style="text-align: center; color: #3232ff;">Tratamentos/Medicamentos</h2>
+        <h2 class="col-sm-12" style="text-align: center; color: #3232ff;">Tratamentos / Medicamentos</h2>
         <div class="accordion col-sm-12 mt-2" id="accordionExample3">
             <div class="card">
                 <div class="card-header" id="headingTwentyFour">
